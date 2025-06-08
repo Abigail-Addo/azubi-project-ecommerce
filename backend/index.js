995f23f304125ef3d15f09f7b298b9bdbbea73b9
Abigail <abigail.addo@bsystemslimited.com>
2025-06-08 16:38:28 +0000

    Updates

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3002",
-      "https://azubi-project-ecommerce.vercel.app/",
+      "https://azubi-project-ecommerce.vercel.app",
     ],
     credentials: true,
   })
